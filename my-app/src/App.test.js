f0ec857c93e89d5c883163703a069f9a2f588a55
phantomcorn <jj.phantakorn@outlook.com>
2022-06-08 22:37:20 +0100

    fix: update test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('can compile main page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/askdoc/i);
   expect(linkElement).toBeInTheDocument();
 });
