d1ec61068bee070ccbd3a269c94c88d0f2e34e96
soham-gadgil <sohamgadgil@googlemail.com>
2022-06-09 04:17:44 +0100

    make it better

--- my-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('can compile main page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/askdoc/i);
-  expect(linkElement).toBeInTheDocument();
-});
