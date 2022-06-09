c3610f1c6571f28b0f2179625eaa3111f7ea750e
phantomcorn <jj.phantakorn@outlook.com>
2022-06-09 21:13:42 +0100

    feat : change Computing.jsx to export function instead of class

--- src/pages/Computing.jsx
+++ src/pages/Computing.jsx
@@ -5,63 +5,6 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-
-
-export class Computing extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            threads: []
-        }
-    }
-
-    componentDidMount() {
-        const dbRef = ref(db, 'Threads');
-        onValue(dbRef, (snapshot) => {
-            let res = readThreadsDB();
-            this.setState({threads : res});
-        })
-        
-
-    }
-
-    render(){
-        return(
-            <div>
-                <h2>
-                    AskDoC
-                </h2>
-                <Container>
-                    <Row>
-                        <Col>
-                            <h4>
-                                Questions:
-                            </h4>
-                            {this.state.threads.map((thread, index) =>{
-                                return (
-                                    <div> 
-                                        <h2> q{index+1} {thread.key} </h2> 
-                                        <div> {thread.data.description} </div>
-                                        <button>Answer this question</button>
-                                    </div>
-                                )
-                            })}
-                        </Col>
-                        <Col>
-                            <h4>Profile (Asker)</h4>
-                            <div> Name : Papa mama</div>
-                            <div> Email : pa@ic.ac.uk </div>
-                            <div> Phone no. : 07552377182 </div>
-                        </Col>
-                    </Row>
-                </Container>
-                    
-            </div>
-        )
-    }
-}
-/*
 export default function Computing() {
     
     const [threads, setThreads] = useState([]);
@@ -70,37 +13,45 @@ export default function Computing() {
 
         const getThreads = async () => {
             let newThreads = readThreadsDB();
-            setThreads(readThreadsDB());
+            setThreads(newThreads);
             console.log(threads);
         } 
         
         getThreads()
         
-    }, []);
+    });
 
 
-    
-            
-    
     return (
         <div>
-            <div>
+            <h2>
                 AskDoC
-            </div>
-            <div> Questions : </div>
-            {threads.map((thread, index) => {
-                return (
-                    <div> 
-                        <h2> {index} {thread.key} </h2> 
-                        <div> {thread.data.description} </div>
-                        <button>Answer this question</button>
-                    
-                    </div>
-                );
-            })}
-            
+            </h2>
+            <Container>
+                <Row>
+                    <Col>
+                        <h4>
+                            Questions:
+                        </h4>
+                        {threads.map((thread, index) =>{
+                            return (
+                                <div> 
+                                    <h2> q{index+1} {thread.key} </h2> 
+                                    <div> {thread.data.description} </div>
+                                    <button>Answer this question</button>
+                                </div>
+                            )
+                        })}
+                    </Col>
+                    <Col>
+                        <h4>Profile (Asker)</h4>
+                        <div> Name : Papa mama</div>
+                        <div> Email : pa@ic.ac.uk </div>
+                        <div> Phone no. : 07552377182 </div>
+                    </Col>
+                </Row>
+            </Container>            
         </div>
     );
 
 }
-*/
