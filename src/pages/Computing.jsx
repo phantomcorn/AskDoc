9b6ac01aa9fb6be4d308c3ee6c1d9571a2088ad0
phantomcorn <jj.phantakorn@outlook.com>
2022-06-09 20:03:58 +0100

    feat : add profile to asker page

--- src/pages/Computing.jsx
+++ src/pages/Computing.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import db, { readThreadsDB } from "../firebase";
-import {getDocs, collection} from "@firebase/firestore";
 import {ref, onValue} from "firebase/database"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 
 export class Computing extends React.Component {
@@ -26,18 +29,34 @@ export class Computing extends React.Component {
     render(){
         return(
             <div>
-                <div>
+                <h2>
                     AskDoC
-                </div>
-                    {this.state.threads.map((thread, index) =>{
-                        return (
-                            <div> 
-                                <h2> {index} {thread.key} </h2> 
-                                <div> {thread.data.description} </div>
-                                <button>Answer this question</button>
-                            </div>
-                        )
-                    })}
+                </h2>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h4>
+                                Questions:
+                            </h4>
+                            {this.state.threads.map((thread, index) =>{
+                                return (
+                                    <div> 
+                                        <h2> q{index+1} {thread.key} </h2> 
+                                        <div> {thread.data.description} </div>
+                                        <button>Answer this question</button>
+                                    </div>
+                                )
+                            })}
+                        </Col>
+                        <Col>
+                            <h4>Profile (Asker)</h4>
+                            <div> Name : Papa mama</div>
+                            <div> Email : pa@ic.ac.uk </div>
+                            <div> Phone no. : 07552377182 </div>
+                        </Col>
+                    </Row>
+                </Container>
+                    
             </div>
         )
     }
