ad1316beab1667f56a8db978bdbb901527bc9f7b
phantomcorn <jj.phantakorn@outlook.com>
2022-06-11 13:14:07 +0100

    fix : remove import

--- frontend/src/pages/Computing.jsx
+++ frontend/src/pages/Computing.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import domain from "../config/host_url"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
--- frontend/src/pages/NonComputing.jsx
+++ frontend/src/pages/NonComputing.jsx
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import domain from "../config/host_url"
 import map from "../assets/map.png"
 import axios from "axios";
 
@@ -22,7 +21,7 @@ export default function NonComputing() {
             title: summary,
             content : detail
         };
-        
+
         const domain = process.env.NODE_ENV === "production" ? "https://drp-askdoc.herokuapp.com/computing" : `http://localhost:5000`
         await axios.post(`${domain}/api/threads`, newThread).then(res => console.log(res.data));
 
