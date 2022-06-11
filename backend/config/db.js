12fefbfd557b83023f69d3e43569cdcffbf8b2ca
phantomcorn <jj.phantakorn@outlook.com>
2022-06-11 02:39:41 +0100

    feat, test : seperate frontend and backend + deploy heroku to backend and run static frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config()
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://admin:DRP_34@cluster0.u9tmfzy.mongodb.net/ThreadsDB")
+        const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
         console.log(`MongoDB connected : ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db")
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,18 @@ connectDB()
 
 // http://<HOST_URL>/api/threads will look up actions in noteRoute.js
 app.use("/api/threads", require("./routes/threadRoute"));
+
+// DEPLOYMENT
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))
+    })
+}
+
 
 app.listen(PORT, function() {
     console.log(
