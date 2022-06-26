75dce334a89d369556fde4fef6290d71b3aeef3f
soham-gadgil <sohamgadgil@googlemail.com>
2022-06-26 18:30:21 +0100

    Added password salts and hashing so plaintext passwords are NOT stored in backend - using bcrypt

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler')
 const Account = require("../models/accountModel")
 const Token = require("../models/tokenModel");
 const sendEmail = require("../utils/sendEmail");
+var bcrypt = require('bcrypt');
 
 // @route POST /api/accounts/signup
 const signup = asyncHandler(async (req, res) => {
@@ -12,34 +13,33 @@ const signup = asyncHandler(async (req, res) => {
 
     const {name,email,password,computing,phone} = req.body
 
-    Account.findOne({email: email}, (err, user) => {
-        res.status(200)
-        
-        if(user) {
-            res.status(400).send({message: "User already registered"})
-        } else {
-            const account = new Account({
-                name,
-                email, 
-                password,
-                computing,
-                phone
-            })
-            account.save(err => {
-                if (err) {
-                    res.status(400).send(err)
-                } else {
-                    res.status(200).send({
-                        name : name,
-                        email: email, 
-                        password: password, 
-                        computing: computing,
-                        phone : phone
-                    })
-                }
-            })
-        }
-    })
+    let user = await Account.findOne({email: email})
+    if(user) {
+        res.status(400).send({message: "User already registered"})
+    } else {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const account = new Account({
+            name,
+            email, 
+            password: hashedPassword,
+            computing,
+            phone
+        })
+        account.save(err => {
+            if (err) {
+                res.status(400).send(err)
+            } else {
+                res.status(200).send({
+                    name : name,
+                    email: email, 
+                    password: hashedPassword, 
+                    computing: computing,
+                    phone : phone
+                })
+            }
+        })
+    }
 })
 
 // @route POST /api/accounts/login
@@ -51,54 +51,17 @@ const login = asyncHandler(async (req, res) => {
 
     const {email,password} = req.body
 
-    Account.findOne({email: email}, (err, user) => {
-        res.status(200)
-        
-        if(user) {
-            if(password === user.password) {
-                res.status(200).send({email: email, password: password, computing: user.computing})
-            } else {
-                res.status(400).send({message: "Incorrect password"})
-            }
+    let user = await Account.findOne({email: email})
+    if(user) {
+        if(await bcrypt.compare(password,user.password)) {
+            res.status(200).send({email: email, password: user.password, computing: user.computing})
         } else {
-            res.status(400).send({message: "User not registered"})
+            res.status(400).send({message: "Incorrect password"})
         }
-    })
+    } else {
+        res.status(400).send({message: "User not registered"})
+    }
 })
-
-// const resetpassword = asyncHandler(async (req, res) => {
-//     if (!req.body.email || !req.body.password) {
-//         res.status(400).send({message: "A field is currently missing"})
-//         throw new Error("A field is currently missing")
-//     }
-
-//     const domain = process.env.NODE_ENV === "production" ? "https://drp-askdoc.herokuapp.com" : `http://localhost:5000`
-
-//     const {email} = req.body
-
-//     let user = await Account.findOne({ email: email });
-//     if (!user)
-//         return res
-//             .status(400)
-//             .send({ message: "User with given email does not exist!" });
-
-//     let token = await Token.findOne({ userId: user._id });
-//     if (!token) {
-//         token = await new Token({
-//             userId: user._id,
-//             token: crypto.randomBytes(32).toString("hex"),
-//         }).save();
-//     }
-
-//     const url = `${domain}/reset-password/${user._id}/${token.token}/`;
-// 		await sendEmail(user.email, "Password Reset", url);
-
-// 		res
-// 			.status(200)
-// 			.send({ message: "Password reset link sent to your email account" });
-
-
-// })
 
 const updateEmail = asyncHandler(async (req, res) => {
     if (!req.body.email) {
@@ -120,13 +83,13 @@ const updateEmail = asyncHandler(async (req, res) => {
 
     let user = await Account.findOne({email: email});
     if (user) {
-        if(password === user.password) {
+        if(await bcrypt.compare(password, user.password)) {
 
             let user = await Account.findOne({email: newemail});
             if (!user) {
                 let updatedUser = await Account.updateOne({email: email}, {$set: {email: newemail}})
                 if (updatedUser) {
-                    res.status(200).send({email: newemail, password: password, computing: updatedUser.computing})
+                    res.status(200).send({email: newemail, password: updatedUser.password, computing: updatedUser.computing})
                 } else {
                     res.status(500).send({message: "Failed to update email"})
                 }
@@ -151,10 +114,10 @@ const updatePassword = asyncHandler(async (req, res) => {
 
     let user = await Account.findOne({email: email});
     if (user) {
-        if(password === user.password) {
+        if(await bcrypt.compare(password, user.password)) {
             let updatedUser = await Account.updateOne({email: email}, {$set: {password: newpassword}})
             if (updatedUser) {
-                res.status(200).send({email: email, password: newpassword, computing: updatedUser.computing})
+                res.status(200).send({email: email, password: updatedUser.password, computing: updatedUser.computing})
             } else {
                 res.status(500).send({message: "Failed to update password"})
             }
@@ -184,9 +147,6 @@ const getAsker = asyncHandler(async (req, res) => {
     }
 })
 
-
-
-
 module.exports = {
-    signup, login, updateEmail, updatePassword, getAsker //resetpassword
+    signup, login, updateEmail, updatePassword, getAsker
 }
